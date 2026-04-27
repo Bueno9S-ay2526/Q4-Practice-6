c204312791f55ad5c5626de5da863e51aa7d6d0c
Alex.com <bueno.zat@obmontessori.edu.ph>
2026-04-28 07:48:29 +0800

    Implement tallyAnimals function for zoo animals
    Add a function to tally and alert animal types in the zoo.

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+function tallyAnimals()  {
+  var animals = ["Bugs", "Fish", "Birds", "Land Animals", "???"];
+  for (var 1 = 0; 1 < animals.length; i++)  {
+  window.alert("This zoo contains a " + animals [i] + ".");
+    }
+}
