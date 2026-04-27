3feb80e64d507630a831b1e53ba886fa2455fd4a
Alex.com <bueno.zat@obmontessori.edu.ph>
2026-04-28 07:51:05 +0800

    Update animals array in tallyAnimals function
    Replaced '???' with 'Unicorn' in the animals array.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function tallyAnimals()  {
-  var animals = ["Bugs", "Fish", "Birds", "Land Animals", "???"];
+  var animals = ["Bugs", "Fish", "Birds", "Land Animals", "Unicorn"];
   for (var 1 = 0; 1 < animals.length; i++)  {
   window.alert("This zoo contains a " + animals [i] + ".");
     }
